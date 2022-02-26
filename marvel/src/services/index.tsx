eff348dcd3a2c221d4d37a724bc68162a2c1949d
zeca.rt22 <zeca.rt22@gmail.com>
2022-02-26 16:22:48 -0300

    creat context

---
+++ marvel/src/services/index.tsx
@@ -0,0 +1,17 @@
+import md5 from "md5";
+
+const publicKey = "6221ced41d8dae9b7c81098342271c5a";
+
+const privateKey = "f80f52e4197b38ba66d4f2944ded284f5046e072";
+
+const dateTime = Number(new Date());
+
+const hash = md5(dateTime + privateKey + publicKey);
+
+const comicsURL = `http://gateway.marvel.com/v1/public/comics?ts=${dateTime}&apikey=${publicKey}&hash=${hash}`;
+
+const heroesURL = `http://gateway.marvel.com/v1/public/characters?ts=${dateTime}&apikey=${publicKey}&hash=${hash}`;
+
+export const apiComics = comicsURL;
+
+export const apiHeroes = heroesURL;
